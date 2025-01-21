2930b1b8e76d1db4c6620b6f452b00f58b85cebd
Simon Chanzu <chanzusimon6@gmail.com>
2025-01-21 14:02:19 +0300

    uploads today

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-
+const apiUrl = "https://noteapi-aqa8.onrender.com"
 const api = axios.create({
     baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
 })
