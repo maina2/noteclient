7ceb4dc4d08901222a57d54e06d1acbcfe97a04f
Simon Chanzu <chanzusimon6@gmail.com>
2025-01-22 17:40:57 +0300

    uploads today

--- src/api.js
+++ src/api.js
@@ -1,22 +1,19 @@
 import axios from 'axios'
 import { ACCESS_TOKEN } from './constants'
 
-const apiUrl = "https://noteapi-aqa8.onrender.com"
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl
-})
+    baseURL: import.meta.env.VITE_API_URL || "https://noteapi-aqa8.onrender.com"
+});
 
 api.interceptors.request.use(
     (config) => {
         const token = localStorage.getItem(ACCESS_TOKEN);
         if (token) {
-            config.headers.Authorization = `Bearer ${token}`
+            config.headers.Authorization = `Bearer ${token}`;
         }
-        return config
+        return config;
     },
-    (error) => {
-        return Promise.reject(error)
-    }
-)
+    (error) => Promise.reject(error)
+);
 
 export default api
